c9082fcb5385d0df5e01080e34ce05a6bce94b80
jesusenriquecabezaacero <jesusenriquecabezaacero@gmail.com>
2026-02-16 16:30:13 -0500

    feat: add Firebase configuration to environment files and initialize Firebase services in the application config.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+   production: false,
+   firebaseConfig: {
+      apiKey: "AIzaSyCFF1mUsJSKMpxQwL8yJuwG8SD0hFsujyM",
+      authDomain: "astro-math-205a4.firebaseapp.com",
+      projectId: "astro-math-205a4",
+      storageBucket: "astro-math-205a4.firebasestorage.app",
+      messagingSenderId: "334779650259",
+      appId: "1:334779650259:web:2350d0926bacb63ad6c426",
+      measurementId: "G-45SHXWPSX7"
+   }
+};
